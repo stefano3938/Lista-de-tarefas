2c1f7208f8b48f05b2f16f9ab87d893070f2d6d3
STEFANO MOROSINI <023.919373@senaicimatec.edu.br>
2025-02-26 18:10:35 +0000

    eu tiha feito erraod kkk, resolvi
    nao precisava do bglh pra puxar  oelemento mais proximo, eu so eswueci de colocar ${} na variavel ainao funcionou

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function adicionar() {
       window.alert('A tarefa atingiu o limite de 100 caracteres')
     }
     else {
-
+      contador++
       let NovoItem = `
   <section id="${contador}" class="ContainerTarefas">
             <div id="CirculoTarefa"><img src="circle_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png" alt=""></div>
             <div id="ConteudoTarefa"><p class="TextoConteudoTarefa">${InputDigiteSuaTarefa.value}</p></div>
-            <div onclick="deletarItem(event)" id="Deletar"><button><img src="delete_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png" alt=""><p>Deletar</p></button></div>
+            <div onclick="deletar(${contador})" id="Deletar"><button><img src="delete_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png" alt=""><p>Deletar</p></button></div>
         </section>`
       AreaLista.innerHTML += NovoItem
       InputDigiteSuaTarefa.value = ''
@@ -33,9 +33,7 @@ InputDigiteSuaTarefa.addEventListener('keyup', function (event) {
   }
 })
 
-function deletarItem(event) {
-
-  let botao = event.currentTarget
-  let elementoMaisProximo = botao.closest('section');
-  elementoMaisProximo.remove()
+function deletar(id){
+var tarefa = document.getElementById(id)
+tarefa.remove()
 }
